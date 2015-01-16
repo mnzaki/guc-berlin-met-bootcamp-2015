cf59d738e2d9d6256ea47f2e74fa0a220d8e0c46
Ahmad Soliman <ahmadadel92@gmail.com>
2015-01-16 23:25:17 +0200

    corrected iterator code

---
+++ design_patterns_code/iterator_pattern_solution/MenuTestDrive.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class MenuTestDrive {
+	public static void main(String args[]) {
+		DinerMenu dinerMenu = new DinerMenu();
+		Waitress waitress = new Waitress(dinerMenu);
+		waitress.printMenu();
+	}
+}
