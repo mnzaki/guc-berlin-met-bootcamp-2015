d605f713054b765d89b95c051c09762c50693eee
Ahmad Soliman <ahmadadel92@gmail.com>
2015-01-21 21:06:30 +0200

    added TODOs to decorator

---
+++ design_patterns_code/decorator_pattern/Soy.java
@@ -0,0 +1,13 @@
+
+public class Soy extends CondimentDecorator {
+
+	// TODO
+	// public Soy(Beverage beverage)
+
+	// TODO
+	// public String getDescription()
+
+	// TODO
+	// public double cost()
+
+}
